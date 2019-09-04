bd933b51eaa4d25ba5fe17bad0f2d05328f20cf3
cacaudev <cacauabreu2013@gmail.com>
2019-09-04 14:13:43 -0300

    [feat] (Locales): Add a few i18n functions for testing interpolation and browser default locale

--- js/global.js
+++ js/global.js
@@ -4,15 +4,28 @@ var set_locale_to = function (locale) {
     $('body').i18n();
 };
 
+jQuery(function () {
+    $.extend($.i18n.parser.emitter, {
+        sitename: () => "App To Test jquery"
+    });
+});
+
 jQuery(function ($) {
     jQuery.i18n().load({
         'en': './i18n/en.json',
         'pt': './i18n/pt.json'
     }).done(function () {
         console.log('Languages loaded!');
-        // url(?locale) =>  takes the value of the locale GET parameter
 
-        // Load page with the browser locale
+        // Browser default language
+        var userLang = navigator.language || navigator.userLanguage;
+        userLang = userLang.split('-')[0]; // In case the locale is like 'en-US', 'pt-BR'
+
+        console.log('Browser language ', userLang);
+
+        if (userLang) set_locale_to(navigator.language)
+
+        // url(?locale) =>  takes the value of the locale GET parameter
         set_locale_to(url('?locale'));
 
         // supports HTML5 History/State APIs (persist locale)
